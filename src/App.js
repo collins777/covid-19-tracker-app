e5cc490a568841d52892958fb17385b19f5055f1
Brandon Collins <brandoncollins@Brandons-iMac.local>
2020-05-27 16:31:46 -0400

    adding code files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+import logo from "./images/image.png";
+import { style } from "@material-ui/system";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: ""
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    console.log(fetchedData);
+    this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async country => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img src={logo} alt="Covid-19 logo" className={styles.image} />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
