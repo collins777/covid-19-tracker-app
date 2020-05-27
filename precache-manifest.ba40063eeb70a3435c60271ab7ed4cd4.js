46df74c0ad9f17ff26322a1b8448ddb68a62b92a
Brandon Collins <brandoncollins@Brandons-iMac.local>
2020-05-27 16:16:56 -0400

    Updates

---
+++ precache-manifest.ba40063eeb70a3435c60271ab7ed4cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7612cc6210d8f5ab576a4b3cbead0ee1",
+    "url": "/covid-19-tracker-app/index.html"
+  },
+  {
+    "revision": "409edb370abd8e1e74cb",
+    "url": "/covid-19-tracker-app/static/css/main.e7538052.chunk.css"
+  },
+  {
+    "revision": "2122b8c5e545116949cb",
+    "url": "/covid-19-tracker-app/static/js/2.9b461807.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker-app/static/js/2.9b461807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409edb370abd8e1e74cb",
+    "url": "/covid-19-tracker-app/static/js/main.92f2da94.chunk.js"
+  },
+  {
+    "revision": "863b8028a661c9ef4548",
+    "url": "/covid-19-tracker-app/static/js/runtime-main.995b9dfc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker-app/static/media/image.d7265326.png"
+  }
+]);
